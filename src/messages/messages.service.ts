cd013200d73991be490c1ddefb5002b9ba3b2640
Matias Nabarro <matiasnabarro@gmail.com>
2022-04-22 00:05:21 +0200

    first steps

---
+++ src/messages/messages.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@nestjs/common";
+import { MessagesRepository } from "./dtos/message.repository";
+
+@Injectable()
+export class MessagesService {
+    constructor (public messagesRepo: MessagesRepository) {}
+
+    findOne(id: string) {
+        return this.messagesRepo.findOne(id);
+    }
+    
+    async findAll() {
+        return this.messagesRepo.findAll();
+    }
+
+    async create(content: string) {
+        return this.messagesRepo.create(content);
+    }
+}
